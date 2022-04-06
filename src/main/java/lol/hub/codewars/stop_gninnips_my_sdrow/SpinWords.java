3a53e46d66ecaf396ac9d364b668de7082ba67a1
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 01:50:31 +0200

    Stop gninnipS My sdroW! javadoc url

--- src/main/java/lol/hub/codewars/stop_gninnips_my_sdrow/SpinWords.java
+++ src/main/java/lol/hub/codewars/stop_gninnips_my_sdrow/SpinWords.java
@@ -3,6 +3,9 @@ package lol.hub.codewars.stop_gninnips_my_sdrow;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+/**
+ * @see <a href="https://www.codewars.com/kata/5264d2b162488dc400000001">codewars.com</a>
+ */
 public class SpinWords {
     private static String spin(String s) {
         if (s.length() < 5) return s;
