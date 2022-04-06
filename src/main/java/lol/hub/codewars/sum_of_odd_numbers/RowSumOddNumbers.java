ad85c1baee4d26318e5c3463c838f849e71d0048
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 01:21:10 +0200

    Sum of odd numbers lul

--- src/main/java/lol/hub/codewars/sum_of_odd_numbers/RowSumOddNumbers.java
+++ src/main/java/lol/hub/codewars/sum_of_odd_numbers/RowSumOddNumbers.java
@@ -2,15 +2,6 @@ package lol.hub.codewars.sum_of_odd_numbers;
 
 public class RowSumOddNumbers {
     public static int rowSumOddNumbers(int n) {
-        int num = 1;
-        for (int row = 1; row <= n; row++) {
-            int sum = 0;
-            for (int j = 1; j <= row; j++) {
-                sum = sum + num;
-                num = num + 2;
-            }
-            if (row == n) return sum;
-        }
-        throw new IllegalStateException();
+        return n * n * n;
     }
 }
