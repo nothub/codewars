a6b477d3c7e655618a42f7f3cd36849f636a501f
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 01:43:04 +0200

    Sort the odd simplify

--- src/main/java/lol/hub/codewars/sort_the_odd/Kata.java
+++ src/main/java/lol/hub/codewars/sort_the_odd/Kata.java
@@ -1,7 +1,6 @@
 package lol.hub.codewars.sort_the_odd;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,23 +8,16 @@ import java.util.List;
  */
 public class Kata {
     public static int[] sortArray(int[] array) {
-        int[] out = new int[array.length];
-        Arrays.fill(out, Integer.MIN_VALUE);
         List<Integer> odd = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                out[i] = array[i];
-            } else odd.add(array[i]);
+        for (int n : array) {
+            if (n % 2 != 0) odd.add(n);
         }
         odd.sort(Integer::compareTo);
-        while (!odd.isEmpty()) {
-            for (int i = 0; i < out.length; i++) {
-                if (out[i] == Integer.MIN_VALUE) {
-                    out[i] = odd.remove(0);
-                    break;
-                }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i] = odd.remove(0);
             }
         }
-        return out;
+        return array;
     }
 }
