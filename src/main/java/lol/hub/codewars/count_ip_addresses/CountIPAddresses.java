e4016b6592f1ffba6eda4b7a7f79682acb9a1fcf
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 13:31:33 +0200

    Count IP Addresses cleanup

--- src/main/java/lol/hub/codewars/count_ip_addresses/CountIPAddresses.java
+++ src/main/java/lol/hub/codewars/count_ip_addresses/CountIPAddresses.java
@@ -1,19 +1,21 @@
 package lol.hub.codewars.count_ip_addresses;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @see <a href="https://www.codewars.com/kata/526989a41034285187000de4">codewars.com</a>
  */
 public class CountIPAddresses {
     public static long ipsBetween(String start, String end) {
-        int[] a = Arrays.stream(start.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        int[] b = Arrays.stream(end.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        long distance = 0L;
-        distance = distance + ((b[0] - a[0]) * 256L * 256L * 256L);
-        distance = distance + ((b[1] - a[1]) * 256L * 256L);
-        distance = distance + ((b[2] - a[2]) * 256L);
-        distance = distance + ((b[3] - a[3]));
-        return distance;
+        return count(end) - count(start);
+    }
+
+    private static long count(String start) {
+        AtomicLong size = new AtomicLong();
+        Arrays.stream(start.split("\\."))
+            .mapToInt(Integer::parseInt)
+            .forEach(i -> size.set(size.get() * 256 + i));
+        return size.get();
     }
 }
