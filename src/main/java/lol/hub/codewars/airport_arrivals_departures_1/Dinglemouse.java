5a945676f4434d2d1f3f5cc9ccfc15ccbb09d357
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 22:57:24 +0200

    Airport Arrivals/Departures - #1

---
+++ src/main/java/lol/hub/codewars/airport_arrivals_departures_1/Dinglemouse.java
@@ -0,0 +1,17 @@
+package lol.hub.codewars.airport_arrivals_departures_1;
+
+import java.util.Arrays;
+
+/**
+ * @see <a href="https://www.codewars.com/kata/57feb00f08d102352400026e">codewars.com</a>
+ */
+class Dinglemouse {
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ?!@#&()|<>.:=-+*/0123456789";
+
+    public static String[] flapDisplay(final String[] lines, final int[][] rotors) {
+        System.out.println(Arrays.toString(lines));
+        System.out.println(Arrays.toString(rotors[0]));
+        // TODO
+        return new String[0];
+    }
+}
---
+++ src/test/java/lol/hub/codewars/airport_arrivals_departures_1/ExampleTests.java
@@ -0,0 +1,31 @@
+package lol.hub.codewars.airport_arrivals_departures_1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ExampleTests {
+    //@Test
+    public void test1() {
+        // HELLO => WORLD!
+        String[] initial = new String[]{"HELLO "};
+        int[][] rotors = new int[][]{{15, 49, 50, 48, 43, 13}};
+        assertArrayEquals(new String[]{"WORLD!"}, Dinglemouse.flapDisplay(initial, rotors));
+    }
+
+    //@Test
+    public void test2() {
+        // CODE => WARS
+        String[] initial = new String[]{"CODE"};
+        int[][] rotors = new int[][]{{20, 20, 28, 0}};
+        assertArrayEquals(new String[]{"WARS"}, Dinglemouse.flapDisplay(initial, rotors));
+    }
+
+    //@Test
+    public void test3() {
+        // CAT => DOG
+        String[] initial = new String[]{"CAT"};
+        int[][] rotors = new int[][]{{1, 13, 27}};
+        assertArrayEquals(new String[]{"DOG"}, Dinglemouse.flapDisplay(initial, rotors));
+    }
+}
