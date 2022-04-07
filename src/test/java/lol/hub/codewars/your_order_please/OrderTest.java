5f00d88da8d9e22cd73a415422f2b49e86444695
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 02:18:52 +0200

    Your order, please update test

--- src/test/java/lol/hub/codewars/your_order_please/OrderTest.java
+++ src/test/java/lol/hub/codewars/your_order_please/OrderTest.java
@@ -2,22 +2,21 @@ package lol.hub.codewars.your_order_please;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class OrderTest {
     @Test
     public void test1() {
-        assertThat(Order.order("is2 Thi1s T4est 3a"), equalTo("Thi1s is2 3a T4est"));
+        assertEquals("Thi1s is2 3a T4est", Order.order("is2 Thi1s T4est 3a"));
     }
 
     @Test
     public void test2() {
-        assertThat(Order.order("4of Fo1r pe6ople g3ood th5e the2"), equalTo("Fo1r the2 g3ood 4of th5e pe6ople"));
+        assertEquals("Fo1r the2 g3ood 4of th5e pe6ople", Order.order("4of Fo1r pe6ople g3ood th5e the2"));
     }
 
     @Test
     public void test3() {
-        assertThat("Empty input should return empty string", Order.order(""), equalTo(""));
+        assertEquals("Empty input should return empty string", "", Order.order(""));
     }
 }
