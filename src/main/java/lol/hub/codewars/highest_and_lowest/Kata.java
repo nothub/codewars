19f43f5d209cc84ea775e8393b94fea029bcad87
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 22:39:50 +0200

    Highest and Lowest SummaryStatistics

--- src/main/java/lol/hub/codewars/highest_and_lowest/Kata.java
+++ src/main/java/lol/hub/codewars/highest_and_lowest/Kata.java
@@ -1,20 +1,17 @@
 package lol.hub.codewars.highest_and_lowest;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.IntSummaryStatistics;
 
 /**
  * @see <a href="https://www.codewars.com/kata/554b4ac871d6813a03000035">codewars.com</a>
  */
 public class Kata {
     public static String highAndLow(String numbers) {
-        Set<Integer> set = Arrays
+        IntSummaryStatistics stats = Arrays
             .stream(numbers.split(" "))
             .mapToInt(Integer::valueOf)
-            .boxed()
-            .collect(Collectors.toUnmodifiableSet());
-        return set.stream().max(Comparator.naturalOrder()).orElseThrow() + " " + set.stream().min(Comparator.naturalOrder()).orElseThrow();
+            .summaryStatistics();
+        return stats.getMax() + " " + stats.getMin();
     }
 }
