3d2a62ff75708efbda8abeb45390ce0cd59d8282
nothub <48992448+nothub@users.noreply.github.com>
2022-04-07 14:11:07 +0200

    Take a Ten Minutes Walk cleanup

--- src/main/java/lol/hub/codewars/take_a_ten_minutes_walk/TenMinWalk.java
+++ src/main/java/lol/hub/codewars/take_a_ten_minutes_walk/TenMinWalk.java
@@ -4,18 +4,20 @@ public class TenMinWalk {
     public static boolean isValid(char[] walk) {
         if (walk.length != 10) return false;
         int x = 0;
-        int z = 0;
+        int y = 0;
         for (char direction : walk) {
             if (direction == 'n') {
-                x = x + 1;
+                x++;
             } else if (direction == 's') {
-                x = x - 1;
+                x--;
             } else if (direction == 'e') {
-                z = z + 1;
+                y++;
             } else if (direction == 'w') {
-                z = z - 1;
+                y--;
+            } else {
+                throw new IllegalArgumentException();
             }
         }
-        return x == 0 && z == 0;
+        return x == 0 && y == 0;
     }
 }
