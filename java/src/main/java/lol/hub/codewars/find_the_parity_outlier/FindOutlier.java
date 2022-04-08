1f7f7ce7b9528985e8ce3baa2eb2be2d75f418cc
nothub <48992448+nothub@users.noreply.github.com>
2022-04-08 20:11:29 +0200

    Find The Parity Outlier

--- java/src/main/java/lol/hub/codewars/find_the_parity_outlier/FindOutlier.java
+++ java/src/main/java/lol/hub/codewars/find_the_parity_outlier/FindOutlier.java
@@ -5,6 +5,21 @@ package lol.hub.codewars.find_the_parity_outlier;
  */
 public class FindOutlier {
     static int find(int[] integers) {
-        return -1;
+        int odds = 0;
+        int evens = 0;
+        int lastOdd = 0;
+        int lastEven = 0;
+        for (int i : integers) {
+            if (i % 2 == 0) {
+                lastEven = i;
+                evens++;
+            } else {
+                lastOdd = i;
+                odds++;
+            }
+            if (evens > 1 && odds == 1) return lastOdd;
+            if (evens == 1 && odds > 1) return lastEven;
+        }
+        throw new IllegalArgumentException();
     }
 }
