acb66432fa4d54aba46e3ab25f8f1bc9b82198c3
nothub <48992448+nothub@users.noreply.github.com>
2022-04-08 18:32:53 +0200

    Moving Zeros To The End

---
+++ python/moving_zeros_to_the_end/main.py
@@ -0,0 +1,9 @@
+def move_zeros(array):
+    """`codewars.com <https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python>`_"""
+    out = []
+    for e in array:
+        if e is not 0:
+            out.append(e)
+    while len(out) < len(array):
+        out.append(0)
+    return out
