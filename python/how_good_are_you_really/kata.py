9cd6097aa430d961695a5f332add7f710140d0cc
nothub <48992448+nothub@users.noreply.github.com>
2022-04-11 20:55:05 +0200

    How good are you really? simplify

--- python/how_good_are_you_really/kata.py
+++ python/how_good_are_you_really/kata.py
@@ -1,7 +1,4 @@
 def better_than_average(class_points: list, your_points: int):
     """`codewars.com <https://www.codewars.com/kata/5601409514fc93442500010b>`_"""
     class_points.append(your_points)
-    avg = sum(class_points) / len(class_points)
-    if your_points > avg:
-        return True
-    return False
+    return your_points > sum(class_points) / len(class_points)
