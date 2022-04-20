9bb9cac50e655c8f0d7db12ea4f6077d33526ab3
nothub <48992448+nothub@users.noreply.github.com>
2022-04-21 00:47:24 +0200

    rot13

--- python/rot13/kata.py
+++ python/rot13/kata.py
@@ -6,9 +6,9 @@ def rot13(message):
     """`codewars.com <https://www.codewars.com/kata/52223df9e8f98c7aa7000062>`_"""
     out = ''
     for c in message:
-        if 'A' < c < 'Z':
+        if 'A' <= c <= 'Z':
             out = out + shift(c, ord('A'))
-        elif 'a' < c < 'z':
+        elif 'a' <= c <= 'z':
             out = out + shift(c, ord('a'))
         else:
             out = out + c
