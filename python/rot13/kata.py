3f9f5a083f33eb4306d6a9d57a70ce96d80cb626
nothub <48992448+nothub@users.noreply.github.com>
2022-04-21 00:45:44 +0200

    rot13

---
+++ python/rot13/kata.py
@@ -0,0 +1,19 @@
+ROT_DISTANCE = 13
+AMOUNT_LETTERS = 26
+
+
+def rot13(message):
+    """`codewars.com <https://www.codewars.com/kata/52223df9e8f98c7aa7000062>`_"""
+    out = ''
+    for c in message:
+        if 'A' < c < 'Z':
+            out = out + shift(c, ord('A'))
+        elif 'a' < c < 'z':
+            out = out + shift(c, ord('a'))
+        else:
+            out = out + c
+    return out
+
+
+def shift(char, index):
+    return chr((ord(char) - index + ROT_DISTANCE) % AMOUNT_LETTERS + index)
