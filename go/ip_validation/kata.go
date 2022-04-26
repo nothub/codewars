5b343b0fd31462fd53bee5ee4d1e12ccc36c0cf7
nothub <48992448+nothub@users.noreply.github.com>
2022-04-26 23:51:19 +0200

    stdlib

--- go/ip_validation/kata.go
+++ go/ip_validation/kata.go
@@ -2,32 +2,11 @@ package ip_validation
 
 // https://www.codewars.com/kata/515decfd9dcfc23bb6000006
 
-import (
-	"log"
-	"regexp"
-	"strconv"
-	"strings"
-)
+import "net"
 
 func Is_valid_ip(ip string) bool {
-	matches, err := regexp.MatchString("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}", ip)
-	if err != nil {
-		log.Fatalln(err)
+	if net.ParseIP(ip) != nil {
+		return true
 	}
-	if !matches {
-		return false
-	}
-	for _, oct := range strings.Split(ip, ".") {
-		if len(oct) > 1 && oct[0:1] == "0" {
-			return false
-		}
-		num, err := strconv.Atoi(oct)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		if num > 255 {
-			return false
-		}
-	}
-	return true
+	return false
 }
