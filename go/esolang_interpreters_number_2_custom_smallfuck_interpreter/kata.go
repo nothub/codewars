f9b2215a82dfb6cadd2ac7be8ffc996b60e87d50
nothub <48992448+nothub@users.noreply.github.com>
2022-04-28 14:12:25 +0200

    JMPB broken

--- go/esolang_interpreters_number_2_custom_smallfuck_interpreter/kata.go
+++ go/esolang_interpreters_number_2_custom_smallfuck_interpreter/kata.go
@@ -23,10 +23,6 @@ func Interpreter(code string, tape string) string {
 	srcp := 0
 	mem := strings.Split(tape, "")
 	memp := 0
-
-	log.Printf("src=%v\n", strings.Join(src, ""))
-	log.Printf("mem=%v\n", strings.Join(mem, ""))
-
 	for {
 		if !pointerValid(srcp, src) {
 			log.Println("exit: code pointer out of range")
@@ -36,9 +32,8 @@ func Interpreter(code string, tape string) string {
 			log.Println("exit: tape pointer out of range")
 			break
 		}
-		inst := src[srcp]
-		log.Printf("memp=%v srcp=%v inst=%s\n", memp, srcp, inst)
-		switch inst {
+		printState(src, srcp, mem, memp)
+		switch src[srcp] {
 		case MOVR:
 			// Move pointer to the right (by 1 cell)
 			memp++
@@ -59,8 +54,7 @@ func Interpreter(code string, tape string) string {
 				for i := srcp; i < len(src); i++ {
 					if src[i] == JMPP {
 						level++
-					}
-					if src[i] == JMPB {
+					} else if src[i] == JMPB {
 						level--
 						if level == 0 {
 							srcp = i + 1
@@ -76,8 +70,7 @@ func Interpreter(code string, tape string) string {
 				for i := len(src) - 1; i >= srcp; i-- {
 					if src[i] == JMPB {
 						level++
-					}
-					if src[i] == JMPP {
+					} else if src[i] == JMPP {
 						level--
 						if level == 0 {
 							srcp = i
@@ -89,8 +82,27 @@ func Interpreter(code string, tape string) string {
 		}
 		srcp++
 	}
-
+	log.Println("final state:")
+	printState(src, srcp, mem, memp)
 	return strings.Join(mem, "")
+}
+
+func printState(src []string, srcp int, mem []string, memp int) {
+	log.Printf("pnt=%v\n", srcp)
+	log.Printf("src=%v\n", strings.Join(src, ""))
+	pointer := "    "
+	for i := 0; i < srcp; i++ {
+		pointer = pointer + " "
+	}
+	log.Println(pointer + "^")
+	log.Printf("pnt=%v\n", memp)
+	log.Printf("mem=%v\n", strings.Join(mem, ""))
+	pointer = "    "
+	for i := 0; i < memp; i++ {
+		pointer = pointer + " "
+	}
+	log.Println(pointer + "^")
+	log.Println("----")
 }
 
 func pointerValid(p int, a []string) bool {
