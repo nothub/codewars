d5a8fe369237be59729122114f566e2343afe711
nothub <48992448+nothub@users.noreply.github.com>
2022-05-03 22:25:22 +0200

    Simple Finite State Machine Compiler

--- java/src/main/java/lol/hub/codewars/simple_finite_state_machine_compiler/FSM.java
+++ java/src/main/java/lol/hub/codewars/simple_finite_state_machine_compiler/FSM.java
@@ -1,7 +1,5 @@
 package lol.hub.codewars.simple_finite_state_machine_compiler;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,13 +9,14 @@ import java.util.Map;
  * @see <a href="https://www.codewars.com/kata/59923f1301726f5430000059">codewars.com</a>
  */
 public class FSM {
-    private final Map<String, Pair<Pair<String, String>, Integer>> transitions = new HashMap<>();
+    private record State(String t0, String t1, Integer o) {}
+    private final Map<String, State> states = new HashMap<>();
 
     public FSM(String instructions) {
-        for (String line : instructions.split("\n")) {
-            String[] split = line.split("; ");
-            String[] nexts = split[1].split(", ");
-            transitions.put(split[0], Pair.of(Pair.of(nexts[0], nexts[1]), Integer.valueOf(split[2])));
+        for (String state : instructions.split("\n")) {
+            String[] parts = state.split("; ");
+            String[] transitions = parts[1].split(", ");
+            states.put(parts[0], new State(transitions[0], transitions[1], Integer.valueOf(parts[2])));
         }
     }
 
@@ -26,10 +25,9 @@ public class FSM {
         path.add(start);
         String state = start;
         for (int i : inputs) {
-            Pair<Pair<String, String>, Integer> t = transitions.get(state);
-            state = (i == 0) ? t.getLeft().getLeft() : t.getLeft().getRight();
+            state = (i == 0) ? states.get(state).t0 : states.get(state).t1;
             path.add(state);
         }
-        return new Result(state, transitions.get(state).getRight(), path.toArray(String[]::new));
+        return new Result(state, states.get(state).o, path.toArray(String[]::new));
     }
 }
