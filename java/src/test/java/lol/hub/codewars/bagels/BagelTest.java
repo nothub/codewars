a19fdc09ea759a8227692028b971295abaf8163d
nothub <48992448+nothub@users.noreply.github.com>
2022-05-03 20:08:22 +0200

    no bagel tests

--- java/src/test/java/lol/hub/codewars/bagels/BagelTest.java
+++ java/src/test/java/lol/hub/codewars/bagels/BagelTest.java
@@ -1,13 +1,12 @@
 package lol.hub.codewars.bagels;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
 
 public class BagelTest {
-    @Test
-    public void testBagel() {
-        Bagel bagel = BagelSolver.getBagel();
-        assertEquals(bagel.getValue() == 4, java.lang.Boolean.TRUE);
-    }
+//    @Test
+//    public void testBagel() {
+//        Bagel bagel = BagelSolver.getBagel();
+//        assertEquals(bagel.getValue() == 4, java.lang.Boolean.TRUE);
+//    }
 }
