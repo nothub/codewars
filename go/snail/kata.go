2caf48f1f465324e17d6b8e3b5366837842390cc
nothub <48992448+nothub@users.noreply.github.com>
2022-05-06 21:43:06 +0200

    cleanup

--- go/snail/kata.go
+++ go/snail/kata.go
@@ -3,43 +3,33 @@ package snail
 // https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
 
 func Snail(arr [][]int) []int {
-	var out []int
-
 	steps := len(arr) - 1
 
 	x := steps
 	y := 0
+
+	var out []int
 	out = append(out, arr[y]...)
 
 	for steps > 0 {
-
-		// y++
 		for i := 0; i < steps; i++ {
-			y++
+			y++ // down
 			out = append(out, arr[y][x])
 		}
-
-		// x--
 		for i := 0; i < steps; i++ {
-			x--
+			x-- // left
 			out = append(out, arr[y][x])
 		}
 		steps--
-
-		// y--
 		for i := 0; i < steps; i++ {
-			y--
+			y-- // up
 			out = append(out, arr[y][x])
 		}
-
-		// x++
 		for i := 0; i < steps; i++ {
-			x++
+			x++ // right
 			out = append(out, arr[y][x])
 		}
-
 		steps--
-
 	}
 
 	return out
