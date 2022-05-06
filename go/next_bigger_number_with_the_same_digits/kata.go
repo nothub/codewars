1114388d5be25b8b55452d7d1e8b994f485e3185
hub <48992448+nothub@users.noreply.github.com>
2022-05-06 14:58:16 +0200

    perms

--- go/next_bigger_number_with_the_same_digits/kata.go
+++ go/next_bigger_number_with_the_same_digits/kata.go
@@ -11,25 +11,26 @@ import (
 func NextBigger(n int) int {
 	s := strconv.Itoa(n)
 	a := strings.Split(s, "")
-	var perms []string
-	Perm(a, &perms)
+	perms := perm(a)
 	log.Println(perms)
 	return 9000
 }
 
-func Perm(arr []string, out *[]string) {
-	perm(arr, 0, out)
+func perm(arr []string) []string {
+	var out []string
+	permRec(arr, 0, &out)
+	return out
 }
 
-func perm(arr []string, i int, out *[]string) {
+func permRec(arr []string, i int, out *[]string) {
 	if i > len(arr) {
 		*out = append(*out, strings.Join(arr, ""))
 		return
 	}
-	perm(arr, i+1, out)
+	permRec(arr, i+1, out)
 	for j := i + 1; j < len(arr); j++ {
 		arr[i], arr[j] = arr[j], arr[i]
-		perm(arr, i+1, out)
+		permRec(arr, i+1, out)
 		arr[i], arr[j] = arr[j], arr[i]
 	}
 }
